97befa6fb17628196c09f107d4fe8266b4b2a617
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-08-28 23:21:21 +0300

    move tests to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,66 @@
 #![feature(negative_impls)]
 #![feature(auto_traits)]
 #![warn(clippy::pedantic)]
+#![allow(clippy::trait_duplication_in_bounds)]
 
 mod property;
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::property::{Access, Has, Property};
+
+    struct Name<'a>(&'a str);
+    struct Age(u8);
+    struct Father<T>(T);
+
+    trait NameOps<'a>: Has<Name<'a>> {
+        fn shout_name(&self) -> String {
+            self.get::<Name>().to_uppercase()
+        }
+    }
+    impl<'a, T: Has<Name<'a>>> NameOps<'a> for T {}
+
+    trait PersonOps<'a>: Has<Name<'a>> + Has<Age> {
+        fn say_hello(&self) -> String {
+            let name = self.get::<Name>();
+            let age = self.get::<Age>();
+            format!("Hi! my name is {name} and I'm {age} years old.")
+        }
+    }
+    impl<'a, T: Has<Name<'a>> + Has<Age>> PersonOps<'a> for T {}
+
+    #[test]
+    fn getting() {
+        let john = (Name("John"), Age(26));
+        assert_eq!(john.shout_name(), "JOHN");
+        assert_eq!(
+            john.say_hello(),
+            "Hi! my name is John and I'm 26 years old."
+        );
+        (Father((Name("Nate"), Age(35))), Name("Mike"), 123, Age(3))
+            .get::<Father<_>>()
+            .say_hello();
+    }
+
+    // Everything below here can be generated with a derive macro
+    impl<'a> Property for Name<'a> {
+        type Item = &'a str;
+        fn get(&self) -> &Self::Item {
+            &self.0
+        }
+    }
+
+    impl Property for Age {
+        type Item = u8;
+        fn get(&self) -> &Self::Item {
+            &self.0
+        }
+    }
+
+    impl<T> Property for Father<T> {
+        type Item = T;
+        fn get(&self) -> &Self::Item {
+            &self.0
+        }
+    }
+}
--- src/property.rs
+++ src/property.rs
@@ -8,12 +8,12 @@ auto trait True {}
 struct IsNot<A, B>(PhantomData<A>, PhantomData<B>);
 impl<T> !True for IsNot<T, T> {}
 
-trait Property {
+pub trait Property {
     type Item;
     fn get(&self) -> &Self::Item;
 }
 
-trait Access {
+pub trait Access {
     fn get<'a, T>(&'a self) -> &'a T::Item
     where
         T: Property + 'a,
@@ -28,7 +28,7 @@ impl<A, B> Access for (A, B) {}
 impl<A, B, C> Access for (A, B, C) {}
 impl<A, B, C, D> Access for (A, B, C, D) {}
 
-trait Has<T>: Access {
+pub trait Has<T>: Access {
     fn _get(&self) -> &T;
 }
 
@@ -127,64 +127,5 @@ where
 {
     fn _get(&self) -> &D {
         &self.3
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    struct Name<'a>(&'a str);
-    struct Age(u8);
-    struct Friend<T: Access>(T);
-
-    impl<'a> Property for Name<'a> {
-        type Item = &'a str;
-        fn get(&self) -> &Self::Item {
-            &self.0
-        }
-    }
-
-    impl Property for Age {
-        type Item = u8;
-        fn get(&self) -> &Self::Item {
-            &self.0
-        }
-    }
-
-    impl<T: Access> Property for Friend<T> {
-        type Item = T;
-        fn get(&self) -> &Self::Item {
-            &self.0
-        }
-    }
-
-    trait NameOps<'a>: Has<Name<'a>> {
-        fn shout_name(&self) -> String {
-            self.get::<Name>().to_uppercase()
-        }
-    }
-    impl<'a, T: Has<Name<'a>>> NameOps<'a> for T {}
-
-    trait PersonOps<'a>: Has<Name<'a>> + Has<Age> {
-        fn is_wise(&self) -> bool {
-            self.get::<Name>().len() > 3 && self.get::<Age>() > &15
-        }
-    }
-    impl<'a, T: Has<Name<'a>> + Has<Age>> PersonOps<'a> for T {}
-
-    #[test]
-    fn getting() {
-        let yoav = (Name("Yoav"), Age(26));
-        assert_eq!(yoav.shout_name(), "YOAV");
-        assert!(yoav.is_wise());
-        (
-            Name("Hi"),
-            "wow",
-            Age(3),
-            Friend((Name("Netanel"), Age(25))),
-        )
-            .get::<Friend<_>>()
-            .is_wise();
     }
 }
